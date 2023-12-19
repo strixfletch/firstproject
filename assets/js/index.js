dfccd7bdc072e507dea82bd7af80ad5c33aba51b
StrixFletch <151624323+strixfletch@users.noreply.github.com>
2023-12-19 20:00:01 +1000

    Привет JavaScript!
    Добавлен js

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+// console.log("Hello world!");
+
+// var a = "1";
+// let b = "2";
+
+// const c = "3";
+
+// console.log(a, b, c);
+
+// a = "То что написано снизу верно";
+// b = "То что написано сверху - ложь";
+
+// console.log(a);
+// console.log(b);
+
+var age = 18
+
+let f = prompt("[РОСКОМНАДЗОР]: Идёт сбор данных, введите как можно больше информации о вас:")
+
+if (age > 17) {
+    console.log("Можно купить");
+} else {
+    console.log("Иди нах№р!");
+}
+
+for (let i = 0; i < 10; i++) {
+        console.log("Ура");
+        
+}
+
+while (j = 10) {
+    console.log("Ура");
+}
