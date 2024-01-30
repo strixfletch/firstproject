f8bede647138c0f96426664eabad585f97f23573
StrixFletch <151624323+strixfletch@users.noreply.github.com>
2024-01-30 19:58:44 +1000

    Опять перемены....

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,13 +4,15 @@ let bool = true
 let nothing = null
 
 
-let man = {
-
-    name: "Ваня",
-    age: "23", 
-    isSick: false,
-    
-}
+let card1text = [
+    {
+        title: "Учёные нашли лекарство от рака!",
+        description: "Шок новость! 2024! ВСЕМ ЧИТАТЬ!",
+        text: "Переведите нам 150 руб, и вам будет отправлено SMS с новостью. 100% не скам, проверено китайцами!",
+        data: "30.01.2024",
+        isPublic: false,
+    },
+];
 
 let h1 = document.getElementsByTagName("h1");
 console.log(h1);
@@ -29,5 +31,22 @@ console.log("background.style.backgroundImage");
 
 let check = document.getElementById("lightmode");
 check.onclick = function() {
-    background.style.backgroundImage = "url(./assets/images/background-dark.png)";
-}
+    console.log('Текущий статус:',check.checked)
+    if (check.checked == true) {
+
+        background.style.backgroundImage = "url(./assets/images/background-dark.png)";
+    
+    } else {
+    
+        background.style.backgroundImage = "url(./assets/images/background-light.png)";
+    
+    }
+};
+
+let cardList = document.querySelector(".cardlist");
+cardList.insertAdjacentHTML("afterbegin", 
+`<h2>${massCards[0].title}</h2>
+<p>36643</p>`
+
+);
+
